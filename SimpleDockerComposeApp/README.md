81fb87d519ec92162f850ab898bcd9c57cbef0f5
TRektFR <78340alex@gmail.com>
2022-09-29 16:13:53 +0200

    first push containing basic structure and fonctionnality of the app. work in progress

---
+++ SimpleDockerComposeApp/README.md
@@ -0,0 +1,5 @@
+### simple application using docker compose, flask, mysql and hopefully nginx
+
+to with docker compose use: docker compose up -d
+
+you should then be able to connect to the app with http://localhost:8000/ and http://localhost:8000/list (it's a work in progress i wrote this down mostly for myself)
---
+++ SimpleDockerComposeApp/backend/hello.py
@@ -0,0 +1,50 @@
+import os
+from flask import Flask, render_template
+import mysql.connector
+
+
+class DBManager:
+    def __init__(self, database='example', host="db", user="root"):
+        self.connection = mysql.connector.connect(
+            user="root", 
+            password="mysql2022",
+            host=host,
+            database=database,
+            auth_plugin='mysql_native_password'
+        )
+        self.cursor = self.connection.cursor()
+    
+    
+    def query_countries_list(self):
+
+        self.cursor.execute('Select * from countries')
+        results = self.cursor.fetchall()
+        return results
+
+server = Flask(__name__)
+conn = None
+
+@server.route('/')
+def index():
+    global conn
+    if not conn:
+    	conn = DBManager()
+    res = conn.query_countries_list()
+    return render_template("index.html")
+
+@server.route('/list')
+def list():
+    global conn
+    if not conn:
+    	conn = DBManager()
+    res = conn.query_countries_list()
+    response = ''
+    for row in res:
+    	response = response +"<div> {0},{1} </div>".format(row[0], row[1]) 
+    return response
+	
+
+
+
+if __name__ == '__main__':
+    server.run()
